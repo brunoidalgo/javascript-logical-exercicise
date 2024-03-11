57266b7385342cddd5ad02e83fb0d4e8e40adb56
SMARAPD - Bruno Empke <BEMPKE@SMARAPD.COM.BR>
2024-03-11 13:06:04 -0300

    Task: Adicionando Lógica

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-alert("Bem Vindo !")
+alert("Bem Vindo ao jogo do número secreto !")
 
-let numeroSecreto = 10
-prompt("Escolha um número entre 1 e 10: ")
+let numeroSecreto = 10;
+let chute;
+let tentativas = 1
+
+// Enquanto o chute não for igual repita o bloco
+while (chute != numeroSecreto) {
+  break;
+  chute = prompt("Escolha um número entre 1 e 10: ");
+
+  if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto} e teve ${tentativas} tentativas`);
+  } 
+  else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que: ${chute}`);
+    } 
+    else {
+      alert(`O número secreto é maior que: ${chute}`);
+    }
+    tentativas ++
+  }
+}
